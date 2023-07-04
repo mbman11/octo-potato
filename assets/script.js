b9ca2051e90015c9e7357cdba5334606bf05ff8a
mbman11 <m.behrmann11@gmail.com>
2023-07-04 04:03:03 -0600

    added coments to the js file

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,17 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+//this function if for when you click the button
+function generatePassword () {
+  console.log (Math.random()); 
+
+
+  
+  return "generated password will go here.";
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -14,4 +23,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
+//below is for the generate button when you click it
 generateBtn.addEventListener("click", writePassword);
