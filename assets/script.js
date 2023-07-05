c78e5b300572701e78d5e20119a3936e6032d510
mbman11 <m.behrmann11@gmail.com>
2023-07-05 06:43:19 -0600

    added arrays, functions, and more

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,64 @@
-// Assignment code here
 
-// Get references to the #generate element
+// start
+
+var characterLength = 8;
+var choiceArr = [];
+var specialChar = ['!','@','#','$','%','^','&','*','(',')',];
+var upperCaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var lowerCaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var numberChar = ['1','2','3','4','5','6','7','8','9','0',];
+
+  // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-
-//this function if for when you click the button
-function generatePassword () {
-  console.log (Math.random()); 
-
-
-  
-  return "generated password will go here.";
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+   var correctPrompts = getPrompts();
+   var passwordText = document.querySelector("#password");
+   if(correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  
+    } else {
+      passwordText.value = '';
 
-  passwordText.value = password;
+    }
+   }
 
+
+
+function generatePassword() {
+  var password = '';
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length)
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
 }
 
-// Add event listener to generate button
+function getPrompts(){
+  choiceArr = [];
+  
+  characterLength = parseInt(prompt('how many characters would you like to have?(atleast 8 up to 128 characters)'));
 
-//below is for the generate button when you click it
-generateBtn.addEventListener("click", writePassword);
+  if(isNaN(characterLength)|| characterLength < 8 || characterLength > 128) {
+    alert('Password needs to be at least 8 characters and no greater than 128 characters, and user input has to be a number.');
+    return false;
+  }
+  if (confirm("Would you like lowercase characters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseChar);
+  }
+  if (confirm("Would you like uppercase characters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseChar);
+  }
+  if (confirm("Would you like numeric characters in your password?")) {
+    choiceArr = choiceArr.concat(numberChar);
+  }
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialChar);
+  }
+  return true;
+}
