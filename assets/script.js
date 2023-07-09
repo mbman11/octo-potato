3912ea7792b388ce71964d18d54aa1b81aadca27
mbman11 <m.behrmann11@gmail.com>
2023-07-09 15:39:13 -0600

    refactored the code and tried to fix cancel button prompt

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,11 @@
 // start
 
 var characterLength = 8;
-var choiceArr = [];
-var specialChar = ['!','@','#','$','%','^','&','*','(',')',];
-var upperCaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
-var lowerCaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
-var numberChar = ['1','2','3','4','5','6','7','8','9','0',];
+var randomArray = [];
+var special = ['!','@','#','$','%','^','&','*','(',')',];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var numbers = ['1','2','3','4','5','6','7','8','9','0',];
 
   // Assignment code here
 var generateBtn = document.querySelector("#generate");
@@ -33,8 +33,8 @@ function writePassword() {
 function generatePassword() {
   var password = '';
   for(var i = 0; i < characterLength; i++) {
-    var randomIndex = Math.floor(Math.random() * choiceArr.length)
-    password = password + choiceArr[randomIndex];
+    var randomIndex = Math.floor(Math.random() * randomArray.length)
+    password = password + randomArray[randomIndex];
   }
   return password;
 }
@@ -44,22 +44,21 @@ function getPrompts(){
   
   characterLength = parseInt(prompt('how many characters would you like to have?(atleast 8 up to 128 characters)'));
 
-  if(isNaN(characterLength)|| characterLength < 8 || characterLength > 128) {
+  if((!characterLength)) {
     alert('Password needs to be at least 8 characters and no greater than 128 characters, and user input has to be a number.');
     return false;
   }
   if (confirm("Would you like lowercase characters in your password?")) {
-    choiceArr = choiceArr.concat(lowerCaseChar);
+    randomArray = randomArray.concat(lowerCase);
   }
   if (confirm("Would you like uppercase characters in your password?")) {
-    choiceArr = choiceArr.concat(upperCaseChar);
+    randomArray = randomArray.concat(upperCase);
   }
   if (confirm("Would you like numeric characters in your password?")) {
-    choiceArr = choiceArr.concat(numberChar);
+    randomArray = randomArray.concat(numbers);
   }
   if (confirm("Would you like special characters in your password?")) {
-    choiceArr = choiceArr.concat(specialChar);
+    randomArray = randomArray.concat(special);
   }
   return true;
 }
-
